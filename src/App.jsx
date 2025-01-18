bb1064df6c4029264936edcc02cfec0c5877c5e5
Vasyl Dutchak <code.genius.web.dev@gmail.com>
2025-01-19 00:12:06 +0100

    combine to main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-
 import './App.css'
+import {lazy} from "react";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./layout/Layout.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import DashboarPage from "./pages/DashboardPage.jsx";
+import Public from "./components/routes/Public.jsx";
+import Private from "./components/routes/Private.jsx";
+import UsersPage from "./pages/UsersPage.jsx";
+import UserInfoPage from "./pages/UserInfoPage.jsx";
+
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage.jsx"));
 
 function App() {
 
 
-  return (
-    <>
-     <h1>Hello from app</h1>
-    </>
-  )
+    return (
+        <Routes>
+            <Route element={<Public/>}>
+                <Route path="/login" element={<LoginPage/>}/>
+            </Route>
+            <Route element={<Private/>}>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="/dashboard" element={<DashboarPage/>}/>
+                    <Route path="/users" element={<UsersPage/>}/>
+                    <Route path="/users/:id" element={<UserInfoPage/>}/>
+                    <Route path='*' element={<NotFoundPage/>}/>
+                </Route>
+            </Route>
+        </Routes>
+    )
 }
 
 export default App
